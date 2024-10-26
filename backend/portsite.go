a8d3b4090a6621606bc09888ca7fae234cebeecb
unknown <john.presley.27@gmail.com>
2024-10-26 18:44:30 -0400

    pulled some gocode into separate files

--- backend/portsite.go
+++ backend/portsite.go
@@ -21,17 +21,8 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("../frontend/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /blog", app.blog)
-	mux.HandleFunc("GET /blog/{id}", app.blogPost)
-	mux.HandleFunc("GET /projects", app.projectsHub)
-	mux.HandleFunc("GET /projects/{id}", app.project)
-
 	logger.Info("starting server", slog.String("addr", ":3000"))
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
---
+++ backend/routes.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("../frontend/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /blog", app.blog)
+	mux.HandleFunc("GET /blog/{id}", app.blogPost)
+	mux.HandleFunc("GET /projects", app.projectsHub)
+	mux.HandleFunc("GET /projects/{id}", app.project)
+
+	return mux
+}
